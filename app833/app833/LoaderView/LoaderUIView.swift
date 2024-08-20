ea57b5efb292ce1dfa201951bf78c868305343ac
Dias <dias.atudinov@gmail.com>
2024-08-20 19:09:28 +0500

    Loader view

--- app833/app833/LoaderView/LoaderUIView.swift
+++ app833/app833/LoaderView/LoaderUIView.swift
@@ -8,8 +8,59 @@
 import SwiftUI
 
 struct LoaderUIView: View {
+    @State private var progress: Double = 0.0
+    @State private var timer: Timer?
+    @State private var isLoadingView: Bool = true
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isLoadingView {
+            ZStack {
+                Color.white
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Spacer()
+                    ZStack {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: .onboardingBtn))
+                            .scaleEffect(x: 1.5, y: 1.5, anchor: .center)
+                            
+                            
+                    }
+                    .foregroundColor(.black)
+                    .padding(14)
+                    .padding(.bottom, 60)
+                }
+                .onAppear {
+                    startTimer()
+                }
+                .onDisappear {
+                    timer?.invalidate()
+                }
+                
+            }
+            
+        } else {
+            if true {
+                ReOnboardingUIView()
+                
+            } else {
+                UsUIView()
+            }
+            
+        }
+    }
+    func startTimer() {
+        timer?.invalidate()
+        progress = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+            if progress < 100 {
+                progress += 1
+            } else {
+                timer.invalidate()
+                isLoadingView.toggle()
+            }
+        }
     }
 }
 
