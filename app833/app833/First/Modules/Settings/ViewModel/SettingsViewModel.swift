3471876c1a956821fc090cab1e05d682bf119f20
Dias <dias.atudinov@gmail.com>
2024-08-20 19:10:53 +0500

    Settings viewModel

--- app833/app833/First/Modules/Settings/ViewModel/SettingsViewModel.swift
+++ app833/app833/First/Modules/Settings/ViewModel/SettingsViewModel.swift
@@ -5,4 +5,30 @@
 //  Created by Dias Atudinov on 20.08.2024.
 //
 
-import Foundation
+import SwiftUI
+import StoreKit
+
+class SettingsViewModel: ObservableObject {
+    let standardNavBarHeight = UIScreen.main.bounds.height / 5.5
+    
+    func shareApp() {
+        guard let url = URL(string: "https://apps.apple.com/app/dafaski/id6633439878") else { return }
+        let activityVC = UIActivityViewController(activityItems: [url], applicationActivities: nil)
+        
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let rootViewController = windowScene.windows.first?.rootViewController {
+            rootViewController.present(activityVC, animated: true, completion: nil)
+        }
+    }
+    
+    func rateApp() {
+        SKStoreReviewController.requestReview()
+    }
+    
+    func openUsagePolicy() {
+        guard let url = URL(string: "https://www.termsfeed.com/live/58023c7e-7f73-46fd-9717-e9b281701e26") else { return }
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+}
