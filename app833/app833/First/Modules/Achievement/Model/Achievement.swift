f0f7adb3ae044320659a16106aecadb7414f9347
Dias <dias.atudinov@gmail.com>
2024-08-26 17:37:44 +0500

    Achievement model part 2

--- app833/app833/First/Modules/Achievement/Model/Achievement.swift
+++ app833/app833/First/Modules/Achievement/Model/Achievement.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct Achievement: Identifiable, Hashable, Codable {
+    var id = UUID()
+    let iconImage: String
+    let image: String
+    let text: String
+    let description: String
+    var complite: Bool
+}
